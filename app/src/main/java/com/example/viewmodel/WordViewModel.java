cf07ea66a051873484fcb122f25850f85f35e170
Frank Rono <frankkiplaa@gmail.com>
2021-06-25 11:33:01 +0300

    Room database example with single word adding to database and show it up on the recyclerview

---
+++ app/src/main/java/com/example/viewmodel/WordViewModel.java
@@ -0,0 +1,31 @@
+package com.example.viewmodel;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.Repository.WordRepository;
+import com.example.entity.Word;
+
+import java.util.List;
+
+public class WordViewModel extends AndroidViewModel {
+
+
+    private WordRepository mRepository;
+
+    private final LiveData<List<Word>> mAllWords;
+
+    public WordViewModel (Application application) {
+        super(application);
+        mRepository = new WordRepository(application);
+        mAllWords = mRepository.getAllWords();
+    }
+
+    public LiveData<List<Word>> getAllWords() { return mAllWords; }
+
+    public void insert(Word word) { mRepository.insert(word); }
+
+
+}
